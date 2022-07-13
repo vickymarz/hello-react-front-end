95b4cdcae66a7d993d0ae09b72d27f866dbe7259
vickymarz <okorojiebube2@gmail.com>
2022-07-12 22:44:30 -0600

    Add greeting component

---
+++ src/pages/Greeting.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '../components/Button'
+
+
+const Greeting = () => {
+  return (
+		<>
+			<h1>Greetings App</h1>
+			<ul>
+				<li>
+					greeting
+				</li>
+			</ul>
+			<Button />
+		</>
+	)
+}
+
+export default Greeting
