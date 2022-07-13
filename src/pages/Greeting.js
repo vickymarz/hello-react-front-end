4edb6fb6f153e19e876b53ef0f57a6a288c80562
vickymarz <okorojiebube2@gmail.com>
2022-07-12 23:28:14 -0600

    Dispatch action to greeting component

--- src/pages/Greeting.js
+++ src/pages/Greeting.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import Button from '../components/Button'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getGreetingList } from '../redux/greetings/greetings'
+import { useState } from 'react'
 
 const Greeting = () => {
+  const greetings = useSelector(state => state.greetings)
+  const dispatch = useDispatch()
+  
+  const [greeting, setGreeting] = useState([])
+
+  const handleGreeting = () => {
+    dispatch(getGreetingList())
+    setGreeting(greetings['greetings'])
+  }
+
+
   return (
 		<>
 			<h1>Greetings App</h1>
 			<ul>
-				<li>
-					greeting
-				</li>
+				<li>{greeting.greeting}</li>
 			</ul>
-			<Button />
+			<Button onClick={handleGreeting} />
 		</>
 	)
 }
